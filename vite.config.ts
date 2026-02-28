423a69b02af7914bfd9c6c0aedc2edbbe62e91de
flobatard <fb.batard@gmail.com>
2026-02-28 16:42:47 +0100

    Lovable removed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
@@ -10,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [svgr(), react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [svgr(), react()].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

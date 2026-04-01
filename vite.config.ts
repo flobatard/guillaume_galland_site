89517c577eff94bf2e6d1ef223752723d52e86ca
flobatard <fb.batard@gmail.com>
2026-04-01 19:15:35 +0200

    Adding prerender

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import svgr from "vite-plugin-svgr";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const vitePrerender = require("vite-plugin-prerender");
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -9,7 +13,14 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [svgr(), react()].filter(Boolean),
+  plugins: [
+    svgr(),
+    react(),
+    vitePrerender({
+      staticDir: path.join(__dirname, "dist"),
+      routes: ["/"],
+    }),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
